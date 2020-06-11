075b4e927f43f3889b9c33a27b9d73397e5bbafe
zackkrida <zackkrida@pm.me>
2020-06-10 22:27:14 -0400

    init

---
+++ pages/_app.js
@@ -0,0 +1,3 @@
+import '../app.css'
+import App from 'next/app'
+export default App
---
+++ pages/blog/example.js
@@ -0,0 +1,89 @@
+export default function BlogPost() {
+  return (
+    <main>
+      <article>
+        <header>
+          <h1>The title of a blog post</h1>
+          <p>
+            A subtitle/lead-in for the post. I guess it'd be like an abstract in
+            an academic paper?
+          </p>
+        </header>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          dolorem ad magnam. Illum iure magni quia perferendis nesciunt autem
+          est rerum debitis accusantium. Suscipit iusto tenetur odio est. Nobis,
+          eum est. Dolorem facilis, perferendis deserunt dolores aspernatur
+          nesciunt recusandae rerum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+          nihil dignissimos voluptate delectus nulla quae voluptatum dolorem
+          laboriosam commodi ipsa.
+        </p>
+        <hr />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde dolor
+          quam qui error sapiente numquam incidunt odio? Repellendus, enim
+          culpa.
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet id
+          mollitia voluptates sit? Voluptas optio iste nobis necessitatibus
+          numquam quae, itaque molestiae quia esse perferendis dignissimos?
+          Placeat nostrum nemo dicta eos facere accusantium inventore tempora
+          quod, voluptatem eaque explicabo in possimus quam beatae velit illum
+          aliquid, dolor eius! Doloremque obcaecati labore et pariatur
+          asperiores fugiat molestias eaque consequuntur in nesciunt!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet id
+          mollitia voluptates sit? Voluptas optio iste nobis necessitatibus
+          numquam quae, itaque molestiae quia esse perferendis dignissimos?
+          Placeat nostrum nemo dicta eos facere accusantium inventore tempora
+          quod, voluptatem eaque explicabo in possimus quam beatae velit illum
+          aliquid, dolor eius! Doloremque obcaecati labore et pariatur
+          asperiores fugiat molestias eaque consequuntur in nesciunt!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet id
+          mollitia voluptates sit? Voluptas optio iste nobis necessitatibus
+          numquam quae, itaque molestiae quia esse perferendis dignissimos?
+          Placeat nostrum nemo dicta eos facere accusantium inventore tempora
+          quod, voluptatem eaque explicabo in possimus quam beatae velit illum
+          aliquid, dolor eius! Doloremque obcaecati labore et pariatur
+          asperiores fugiat molestias eaque consequuntur in nesciunt!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet id
+          mollitia voluptates sit? Voluptas optio iste nobis necessitatibus
+          numquam quae, itaque molestiae quia esse perferendis dignissimos?
+          Placeat nostrum nemo dicta eos facere accusantium inventore tempora
+          quod, voluptatem eaque explicabo in possimus quam beatae velit illum
+          aliquid, dolor eius! Doloremque obcaecati labore et pariatur
+          asperiores fugiat molestias eaque consequuntur in nesciunt!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet id
+          mollitia voluptates sit? Voluptas optio iste nobis necessitatibus
+          numquam quae, itaque molestiae quia esse perferendis dignissimos?
+          Placeat nostrum nemo dicta eos facere accusantium inventore tempora
+          quod, voluptatem eaque explicabo in possimus quam beatae velit illum
+          aliquid, dolor eius! Doloremque obcaecati labore et pariatur
+          asperiores fugiat molestias eaque consequuntur in nesciunt!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet id
+          mollitia voluptates sit? Voluptas optio iste nobis necessitatibus
+          numquam quae, itaque molestiae quia esse perferendis dignissimos?
+          Placeat nostrum nemo dicta eos facere accusantium inventore tempora
+          quod, voluptatem eaque explicabo in possimus quam beatae velit illum
+          aliquid, dolor eius! Doloremque obcaecati labore et pariatur
+          asperiores fugiat molestias eaque consequuntur in nesciunt!
+        </p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+        nulla.
+      </article>
+    </main>
+  )
+}
